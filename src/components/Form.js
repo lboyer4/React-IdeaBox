7463629cfad6566abe55316e9f27dd41909f3c98
Lauren Boyer <lboyer4@outlook.com>
2019-04-28 15:54:29 -0600

    made ideas render
    Co-Authored By: Ryan Barnett <conner2468@gmail.com>

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,18 +3,29 @@ import '../styles/_Form.scss';
 
 
 class Form extends Component {
+	 // constructor(props) {
+  //   super(props) 
+  //   this.state = {
+  //   }
+  // }
 
-	
+	clickHandler = (e) => {
+		let title =e.target.parentElement.querySelector('.title').value;
+		let body = e.target.parentElement.querySelector('.body').value
+		this.props.createNewIdea(title, body)
+	}
 
 	render() {
 		return (
-			<form>
-				<label for="title">Title</label>
-      		<textarea rows="2" cols="50" class="title" type="text" name="title"></textarea>
-      	<label for="body">Body</label>
-      		<textarea rows="4" cols="50" class="body-style input-selector" type="text" name="Body"></textarea>
-      	<button id="save-btn" class="save-btn"type="button" name="Save">Save</button>
-			</form>
+			<main className="form-holder">
+				<form>
+					<label for="title">Title</label>
+	      		<textarea rows="2" cols="50" className="title" type="text" name="title"></textarea>
+	      	<label for="body">Body</label>
+	      		<textarea rows="4" cols="50" className="body" type="text" name="Body"></textarea>
+	      	<button onClick={this.clickHandler} className="save-btn"type="button" name="Save">Save</button>
+				</form>
+			</main>
 		)
 	}
 }
