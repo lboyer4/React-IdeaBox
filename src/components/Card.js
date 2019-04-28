5d0cae4e8083bf3c19ec17ef59f5e0d2164017f0
Lauren Boyer <lboyer4@outlook.com>
2019-04-28 17:15:25 -0600

    deletes card
    Co-Authored By: Ryan Barnett <conner2468@gmail.com>

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,19 @@ import React from 'react';
 import '../styles/_Card.scss';
 
 function Card(props) {
+
+function handleClick() {
+	props.deleteIdea(props.id)
+}
+
 		return (
 			<article>
 				<h2>{props.title}</h2>
 				<p>{props.body}</p>
-				
+				<button className="delete-btn" onClick={handleClick}>X</button>
 			</article>
 		)
 	}
+
 
 export default Card;
